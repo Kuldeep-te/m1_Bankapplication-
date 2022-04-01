868fabb71ce5216ca12d8e94b5d39cb03a0d36fc
Kuldeep <77101903+Kuldeep-te@users.noreply.github.com>
2022-04-01 12:28:15 +0530

    Create struct.c

---
+++ 3_Implementation/src/struct.c
@@ -0,0 +1,8 @@
+struct acc_type
+{
+     char bank_name[20];
+     char bank_branch[20];
+     char acc_holder_name[30];
+     int acc_number;
+     char acc_holder_address[100];
+     float available_balance;
